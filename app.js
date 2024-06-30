4761d2e21dd7c8ffe5d208fa546c1d7cebc451ab
Anubhav <110095813+Anubhav-pandey004@users.noreply.github.com>
2024-06-30 21:28:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const User = require('./models/userModel')
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 app.use(express.json())
 app.use(cookieParser());
 let bodyParser = require('body-parser');
